8b8435bd5532fbd3ea5ddd434e315ae050739a99
java_tsi08 <varturs81@gmail.com>
2018-11-08 19:37:02 +0200

    new_08112018battle_vb1

--- src/main/java/lv/tsi/battleship/RegistrationServlet.java
+++ src/main/java/lv/tsi/battleship/RegistrationServlet.java
@@ -5,7 +5,8 @@ import java.io.IOException;
 @javax.servlet.annotation.WebServlet(name = "RegistrationServlet", urlPatterns = "/registration" )
 public class RegistrationServlet extends javax.servlet.http.HttpServlet {
     protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
-
+        String un = request.getParameter("username");
+        System.out.println(un);
     }
 
     protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
