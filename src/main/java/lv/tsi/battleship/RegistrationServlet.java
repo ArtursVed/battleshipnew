3dd0e817898abeb468a2a00c548c022cbe80c50d
java_tsi08 <varturs81@gmail.com>
2018-11-08 20:50:17 +0200

    new_08112018battle_vb2

--- src/main/java/lv/tsi/battleship/RegistrationServlet.java
+++ src/main/java/lv/tsi/battleship/RegistrationServlet.java
@@ -1,12 +1,23 @@
 package lv.tsi.battleship;
 
+import lv.tsi.battleship.model.MyGame;
+import lv.tsi.battleship.model.User;
+
+import javax.inject.Inject;
 import java.io.IOException;
 
 @javax.servlet.annotation.WebServlet(name = "RegistrationServlet", urlPatterns = "/registration" )
 public class RegistrationServlet extends javax.servlet.http.HttpServlet {
+
+    @Inject
+    private MyGame myGame;
+
     protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
         String un = request.getParameter("username");
-        System.out.println(un);
+        User user = new User();
+        user.setName(un);
+        myGame.setUser(user);
+        response.sendRedirect("/battleship/waitregistration");
     }
 
     protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
---
+++ src/main/webapp/WEB-INF/pages/waitregistration.jsp
@@ -0,0 +1,19 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: java_tsi08
+  Date: 08.11.2018
+  Time: 20:31
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+    <meta http-equiv="refresh" content="3">
+</head>
+<body>
+
+<h1>${myGame.user.name} Please wait another player</h1>
+
+</body>
+</html>
