f3c3233378cb1847ddd99af5035e9f22ac954817
java_tsi08 <varturs81@gmail.com>
2018-11-08 19:21:38 +0200

    new_08112018battle

---
+++ src/main/java/lv/tsi/battleship/RegistrationServlet.java
@@ -0,0 +1,14 @@
+package lv.tsi.battleship;
+
+import java.io.IOException;
+
+@javax.servlet.annotation.WebServlet(name = "RegistrationServlet", urlPatterns = "/registration" )
+public class RegistrationServlet extends javax.servlet.http.HttpServlet {
+    protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+
+    }
+
+    protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+        request.getRequestDispatcher("/WEB-INF/pages/registration.jsp") .include(request,response);
+    }
+}
